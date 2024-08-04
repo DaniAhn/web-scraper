5f0058048f348080a1402690fb08696df6d5040a
Daniel Ahn <danielahn324@gmail.com>
2024-08-03 19:22:38 -0700

    Implemented csv data output and scatterplot

--- web-scraper.py
+++ web-scraper.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import matplotlib
+import matplotlib.pyplot as plt
 import lxml
 import csv
 
 class HomeListing:
-    def __init__(self, address, price, sqft, bd, ba):
+    def __init__(self, address: str, price: str, sqft: str, bd: str, ba: str):
         self.address = address
         self.price = price
         self.sqft = sqft
@@ -30,6 +30,8 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
 }
 
+home_listings = []
+
 for page in range(start_page, end_page + 1):
     url = base_url.format(city = city, page = page)
     print(url)
@@ -41,11 +43,8 @@ for page in range(start_page, end_page + 1):
         html_content = response.content
 
         html_doc = BeautifulSoup(html_content, "lxml")
-        print(f"{html_doc.prettify()}\n")
 
         listing_container = html_doc.find_all("li", class_="ListItem-c11n-8-102-0__sc-13rwu5a-0 StyledListCardWrapper-srp-8-102-0__sc-wtsrtn-0 hKdzLV kgwlbT")
-
-        home_listings = []
 
         for home in listing_container:
             address_tag = home.find('address')
@@ -65,19 +64,44 @@ for page in range(start_page, end_page + 1):
                         bathrooms = li_elements[1].text.strip().split()[0]
                         square_feet = li_elements[2].text.strip().split()[0]
 
-                        home_listing = HomeListing(address, price, square_feet, bedrooms, bathrooms)
+                        home_listing = HomeListing(address, price[2:], square_feet, bedrooms, bathrooms)
+                        print(f"Address: {home_listing.address}")
+                        print(f"Price: {home_listing.price}")
+                        print(f"Sqft: {home_listing.sqft}")
+                        print(f"Bd: {home_listing.bd}")
+                        print(f"Ba: {home_listing.ba}")
+                        print("\n")
+
                         home_listings.append(home_listing)
 
-        with open("home_listings.csv", "w") as csv_file:
-            fieldnames = ["Address", "Price", "Square Feet", "Bedrooms", "Bathrooms"]
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-            writer.writeheader()
-            for listing in home_listings:
-                writer.writerow({
-                    "Address": listing.address, 
-                    "Price": listing.price, 
-                    "Square Feet": listing.sqft, 
-                    "Bedrooms": listing.bd, 
-                    "Bathrooms": listing.ba
-                })
+with open("home_listings.csv", "w") as csv_file:
+    fieldnames = ["Address", "Price", "Square Feet", "Bedrooms", "Bathrooms"]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    for listing in home_listings:
+        writer.writerow({
+            "Address": listing.address,
+            "Price": f"${listing.price}", 
+            "Square Feet": listing.sqft, 
+            "Bedrooms": listing.bd, 
+            "Bathrooms": listing.ba
+        })
 
+for listing in home_listings:
+    if listing.sqft == "--":
+        home_listings.remove(listing)
+
+sqft_dataset = [int((listing.sqft).replace(",", "")) for listing 
+                in home_listings]
+price_dataset = [int((listing.price).replace(",", "")) for listing 
+                 in home_listings]
+
+plt.scatter(sqft_dataset, price_dataset)
+
+plt.xlabel("Square Feet")
+plt.ylabel("Price (USD)")
+plt.title("Price of Homes on Zillow in Victoria, BC by Square Feet")
+
+plt.grid(True)
+
+plt.show()
